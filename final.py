b7e44939b44837fa03de451dadd3ce1d62c6b1cb
dvdplayer26 <davidl526@nycstudents.net>
2026-03-23 12:46:29 +0000

    v1.01

--- final.py
+++ final.py
@@ -1,7 +1,9 @@
 import random
 import time
 wordList = ["apple", "banana", "carrot", "durian", "eggplant", "fruit", ]
+senList = ["Apple seeds need cold stratification before being planted.", "A banana is scientifically considered a berry.", "A carrot is actually the root of the carrot plant!", "People say durians do not smell very pleasant.", "Not many people cook eggplant nowadays.", "Peppers, Tomatoes, and Cucumbers are actually fruits!", ]
 word = random.choice(wordList)
+sen = random.choice(senList)
 
 def Error1():
     print("Invalid choice!")
@@ -20,8 +22,20 @@ def wordType():
     time.sleep(2)
     print("Ending session...")
 
+def senType():
+    isWrong = False
+    while isWrong == False:
+        sen = random.choice(senList)
+        print(sen)
+        typedSen = input()
+        if typedSen != sen:
+            print ("Wrong!")
+            isWrong = True
+    time.sleep(2)
+    print("Ending session...")
+
 def keyboardSkill():
-    print("Welcome user. This is a keyboard skill check. \nSelect what you would like to do. \na) Word typing practice \nb) Sentence type practice \nc) Paragraph type practice")
+    print("Welcome user. This is a keyboard skill check. \nSelect what you would like to do. \na) Word typing practice \nb) Sentence type practice")
     menu_choice = input()
     if menu_choice == "a":
         wordType()
